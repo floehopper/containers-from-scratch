78421ad8effd3b973720b9f982275b7c28172060
James Mead <james@floehopper.org>
2017-12-30 15:00:20 +0000

    Provision VM with basic packages including Ruby v2.4

---
+++ provision.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+apt-add-repository ppa:brightbox/ruby-ng
+apt-get update --yes
+apt-get --yes install software-properties-common
+apt-get --yes install ruby2.4
+apt-get --yes install ruby2.4-dev
+apt-get --yes install g++
