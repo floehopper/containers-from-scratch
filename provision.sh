cafd8fc228480d63783aa75d906446ee075dc691
James Mead <james@floehopper.org>
2017-12-31 02:20:15 +0000

    Chroot into a basic Ubuntu filesystem in 'child' process
    Note that the `UBUNTU_CONTAINER_ROOT` file is intended as a marker to
    make it easier to work out which root directory we are in.
    
    You can see that the bash shell is running with the new root filesystem:
    
        root@ubuntu-xenial:/vagrant# ./container run /bin/bash
        run: Running ["/bin/bash"] as PID 14093
        child: Running ["/bin/bash"] as PID 14095
        grandchild: Running ["/bin/bash"] as PID 1
        root@container:/# pwd
        /
        root@container:/# ls
        UBUNTU_CONTAINER_ROOT  bin  boot  dev  etc  home  lib  lib64  media  mnt
        opt  proc  root  run  sbin  srv  sys  tmp  usr  var
        root@container:/# exit
        root@ubuntu-xenial:/vagrant#

--- provision.sh
+++ provision.sh
@@ -7,5 +7,12 @@ apt-get --yes install ruby2.4
 apt-get --yes install ruby2.4-dev
 apt-get --yes install g++
 gem install bundler
+rm -rf ubuntufs
+mkdir ubuntufs
+cd ubuntufs
+wget -q http://cdimage.ubuntu.com/ubuntu-base/releases/16.04/release/ubuntu-base-16.04-core-amd64.tar.gz
+tar xfz ubuntu-base-16.04-core-amd64.tar.gz
+rm ubuntu-base-16.04-core-amd64.tar.gz
+touch UBUNTU_CONTAINER_ROOT
 cd /vagrant
 bundle install
